ca263601ba35692b2330de2c69a6f8c999799d88
akannac <mcchandzz1@gmail.com>
2016-12-31 00:53:05 +0100

    assign4-soln

---
+++ angularJS/assignment4/src/items.controller.js
@@ -0,0 +1,16 @@
+
+(function () {
+    'use strict';
+
+    angular.module('MenuApp')
+        .controller('ItemsController', ItemsController);
+
+    ItemsController.$inject = ['itemsList'];
+    function ItemsController(itemList) {
+        // console.info('ItemsController:', itemList);
+
+        var ctrl = this;
+        ctrl.items = itemList.menu_items;
+        ctrl.categoryName = itemList.category.name;
+    }
+})()
---
+++ angularJS/assignment4/src/routes.js
@@ -0,0 +1,47 @@
+
+(function () {
+    'use strict';
+
+    angular.module('MenuApp')
+        .config(RoutesConfig);
+
+    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function RoutesConfig($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'src/templates/home.template.html'
+            })
+            .state('categories', {
+                url: '/categories',
+                templateUrl: 'src/templates/categories.template.html',
+                controller: 'CategoryController',
+                controllerAs: 'ctrl',
+                resolve: {
+                    categoriesList: ['MenuDataService', function (MenuDataService) {
+                        var result = MenuDataService.getAllCategories();
+                        // console.log('RoutesConfig.state.categories:', result);
+
+                        return result;
+                    }]
+                }
+            })
+            .state('items', {
+                url: '/items/{shortname}',
+                templateUrl: 'src/templates/items.template.html',
+                controller: 'ItemsController',
+                controllerAs: 'ctrl',
+                resolve: {
+                    itemsList: ['$stateParams', 'MenuDataService', function ($stateParams, MenuDataService) {
+                        var result =  MenuDataService.getItemsForCategory($stateParams.shortname);
+                        // console.log('RoutesConfig.$stateParams.shortname:', $stateParams.shortname)
+                        // console.log('RoutesConfig.state.items:', result);
+
+                        return result;
+                    }],
+                }
+            });
+    }
+})()
